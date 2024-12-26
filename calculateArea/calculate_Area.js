ebdbb3ec83caccdea24a0859036185e02f4c3fc7
Andres Barrios <andres@generaweb.net>
2024-12-26 18:26:51 -0500

    grocery calculator

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,5 @@ function calculateArea() {
     let area = length * width;
      document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let groceryTracker = () => document.getElementById('resultGrocery').innerHTML="The total amount is: $ "+parseFloat(document.getElementById('grocery1').value)+parseFloat(document.getElementById('grocery2').value)+parseFloat(document.getElementById('grocery3').value);
